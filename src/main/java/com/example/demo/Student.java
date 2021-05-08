c33b0a2786079b0c2b4ee59c45eb3f461514e2a4
Molly Huang <molly.h@gmail.com>
2021-05-08 19:40:52 +0800

    44:19​ @Table and Constraints -  student_email_unique

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -3,6 +3,15 @@ package com.example.demo;
 import javax.persistence.*;
 
 @Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "student_email_unique",
+                        columnNames = "email"
+                )
+        }
+)
 public class Student {
 
     @Id
@@ -39,8 +48,7 @@ public class Student {
     @Column(
             name = "email",
             nullable = false,
-            columnDefinition = "TEXT",
-            unique = true
+            columnDefinition = "TEXT"
     )
     private String email;
 
